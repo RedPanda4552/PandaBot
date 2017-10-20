bc9b82ed7712108e017d66e4d68c50f5d8489ff7
Brian <redpanda4552@gmail.com>
2017-10-20 00:24:30 -0400

    Unused import

--- src/main/java/io/github/redpanda4552/PandaBot/PandaBot.java
+++ src/main/java/io/github/redpanda4552/PandaBot/PandaBot.java
@@ -41,7 +41,6 @@ import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
-import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.VoiceChannel;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 
